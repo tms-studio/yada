4fa0992af48bb009da543bf35697725e179db09f
Sylvan Le Deunff <sylvan.ledeunff@orange.com>
2021-05-05 17:17:04 +0200

    Repeat 1000 times: GitHub isn't an IDE

--- app/application.py
+++ app/application.py
@@ -2,11 +2,11 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
   context = {}
   
-  if method == 'POST' and request.form.get('username', None):
+  if request.method == 'POST' and request.form.get('username', None):
     context['username'] = request.form['username']
     
   return render_template('index.html', **context)
